9192afcb6a95010176ae19ad15505aedf3046373
BigBarfEaterBBE <ajiang9@bostonk12.org>
2025-12-29 14:28:48 -0500

    12/29 2:28 PM O adjusted the controls for each view and tried drawing grid lines

--- supermarket_simulator/scripts/grid_root.gd
+++ supermarket_simulator/scripts/grid_root.gd
@@ -1,15 +1,39 @@
 extends Node3D
 @export var grid_size := Vector3i(6, 4, 5)
 @export var cell_size := 1.0
+@export var camera_node_path: NodePath
+var camera_ref: Camera3D
 
 var grid: Grid3D
 var cursor := Vector3i.ZERO
 var current_item := ItemData.new(1, "Milk", Vector3i(1,3,1))
 var preview
 var offset: Vector3
+var move_map = [
+	{"up": Vector3i(-1, 0, 0), "down": Vector3i(1, 0, 0), "left": Vector3i(0, 0, 1), "right": Vector3i(0, 0, -1)}, # top
+	{"up": Vector3i(0,1,0),  "down": Vector3i(0,-1,0), "left": Vector3i(1,0,0), "right": Vector3i(-1,0,0)}, # front
+	{"up": Vector3i(0,1,0),  "down": Vector3i(0,-1,0), "left": Vector3i(0,0,-1), "right": Vector3i(0,0,1)}  # side
+]
+
+func draw_grid_lines():
+	var line_mat := StandardMaterial3D.new()
+	line_mat.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	line_mat.albedo_color = Color(0.5, 0.5, 0.5)
+	
+	for x in range(grid_size.x + 1):
+		for z in range(grid_size.z + 1):
+			var line = MeshInstance3D.new()
+			var mesh := BoxMesh.new()
+			mesh.size = Vector3(0.05, grid_size.y * cell_size, 0.05)
+			line.mesh = mesh
+			line.material_override = line_mat
+			line.position = Vector3(x * cell_size, grid_size.y * cell_size * 0.5, z* cell_size) - offset
+			$GridLines.add_child(line)
+			
 
 #create grid cubes
 func _ready():
+	camera_ref = get_node(camera_node_path) as Camera3D
 	offset = Vector3( (grid_size.x - 1) * 0.5, (grid_size.y - 1) *0.5, (grid_size.z - 1) * 0.5) * cell_size
 	preview = preload("res://supermarket_simulator/scenes/item_preview.tscn").instantiate()
 	preview.item_size = current_item.size
@@ -21,6 +45,7 @@ func _ready():
 			for z in grid_size.z:
 				var cube = MeshInstance3D.new()
 				cube.mesh = BoxMesh.new()
+				#create material
 				var mat := StandardMaterial3D.new()
 				mat.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
 				mat.cull_mode = BaseMaterial3D.CULL_DISABLED
@@ -29,17 +54,19 @@ func _ready():
 				cube.scale = Vector3.ONE * 0.95
 				cube.position = (Vector3(x, y, z) * cell_size) - offset
 				$Cells.add_child(cube)
+	draw_grid_lines()
 
 #movement
 func _process(_delta):
+	var map = move_map[camera_ref.view_index]
 	if Input.is_action_just_pressed("move_left"):
-		cursor.x -= 1
+		cursor += map["left"]
 	if Input.is_action_just_pressed("move_right"):
-		cursor.x += 1
+		cursor += map["right"]
 	if Input.is_action_just_pressed("move_forward"):
-		cursor.z -= 1
+		cursor += map["up"]
 	if Input.is_action_just_pressed("move_back"):
-		cursor.z += 1
+		cursor += map["down"]
 	
 	cursor = cursor.clamp(Vector3i.ZERO, grid_size - Vector3i.ONE)
 	
